7107cc101762c85ff73d947765db09c70c165185
Fernando Rosenblit <fernandorosenblit@gmail.com>
2021-09-14 11:14:26 -0300

    Add analyze bundle

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+	enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
 	reactStrictMode: true,
 	images: {
-    domains: ['cdn.images.dev.external.hollywood.com'],
-  },
+		domains: ["cdn.images.dev.external.hollywood.com"],
+	},
 	i18n: {
 		locales: ["en-us"],
 		defaultLocale: "en-us",
 	},
-};
+});
